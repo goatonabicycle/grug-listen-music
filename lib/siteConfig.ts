30da4300c48d15114f7694f79f250618ee34d4d6
Rowan <malice.peanutbutter@gmail.com>
2023-10-22 22:49:43 +0200

    Links updated. Dates

--- lib/siteConfig.ts
+++ lib/siteConfig.ts
@@ -2,13 +2,13 @@ const siteConfig = {
   title: "Grug Listen Music",
   description: "This will eventually be a real big boy website!",
   author: "Grug",
-  url: "https://nextjs-contentlayer-tailwind.vercel.app",
-  siteImage: "/images/announcement-banner.png",
+  url: "https://gruglistenmusic.com",
+  siteImage: "/images/grug-face.png",
   socials: {
     twitter: "https://twitter.com/",
     telegram: "https://t.me/",
-    email: "GottaMakeThisEmail@protonmail.com",
-    site: "grugmusiclisten.com",
+    email: "gruglistenmusic@protonmail.com",
+    site: "gruglistenmusic.com",
   },
   blog: {
     postPerPage: 9,
