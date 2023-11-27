fcf263d02b12552b68b197cd69168712cfc49f73
Rowan <malice.peanutbutter@gmail.com>
2023-11-27 19:55:35 +0200

    Pal page now much more dynamic.

--- app/pals/page.tsx
+++ app/pals/page.tsx
@@ -1,6 +1,96 @@
 import Image from "@/components/image";
 
 const Pals = () => {
+  const pals = [
+    {
+      name: "Last Rites",
+      url: "https://yourlastrites.com/",
+    },
+    {
+      name: "CVLTNation",
+      url: "https://cvltnation.com/",
+    },
+    {
+      name: "Noob Heavy",
+      url: "https://noobheavy.com/",
+    },
+    {
+      name: "Toilet ov Hell",
+      url: "https://toiletovhell.com/",
+    },
+    {
+      name: "Small Albums",
+      url: "https://www.smallalbums.com/",
+    },
+    {
+      name: "Machine Music",
+      url: "https://machinemusic.net/",
+    },
+    {
+      name: "For the Rabbits",
+      url: "https://fortherabbits.net/",
+    },
+    {
+      name: "Gold Flake Paint",
+      url: "https://www.goldflakepaint.co.uk/",
+    },
+    {
+      name: "Angry Metal Guy",
+      url: "https://www.angrymetalguy.com/",
+    },
+    {
+      name: "Echoes and Dust",
+      url: "https://echoesanddust.com/",
+    },
+    {
+      name: "No Clean Singing",
+      url: "https://www.nocleansinging.com/",
+    },
+    {
+      name: "Invisible Oranges",
+      url: "https://www.invisibleoranges.com/",
+    },
+    {
+      name: "Teeth of the Divine",
+      url: "https://www.teethofthedivine.com/",
+    },
+    {
+      name: "Everything Is Noise",
+      url: "https://everythingisnoise.net/",
+    },
+    {
+      name: "Heavy Blog Is Heavy",
+      url: "https://www.heavyblogisheavy.com/",
+    },
+    {
+      name: "Various Small Flames",
+      url: "https://varioussmallflames.co.uk/",
+    },
+    {
+      name: "Can This Even Be Called Music?",
+      url: "https://canthisevenbecalledmusic.com/",
+    },
+  ];
+
+  const others = [
+    {
+      name: "KManriffs",
+      url: "https://twitter.com/KManriffs",
+    },
+    {
+      name: "Audio Antihero",
+      url: "http://audioantihero.com/",
+    },
+    {
+      name: "WereGnome Records",
+      url: "https://weregnomerecords.bandcamp.com/",
+    },
+    {
+      name: "Sentient Ruin Laboratories",
+      url: "http://sentientruin.com/main",
+    },
+  ];
+
   return (
     <div className="min-h-screen flex flex-col gap-4 justify-center items-center">
       <Image
@@ -10,6 +100,7 @@ const Pals = () => {
         priority
         sizes="(min-width: 1200px) 100vw, 100vw"
       />
+
       <div className="px-10 py-20 text-center w-full">
         <p className="mb-4">
           Grug have many pals look up to. <br />
@@ -22,96 +113,24 @@ const Pals = () => {
             <p className="mb-4">
               Pals do actual reviews and words. <br />
             </p>
-            <a target="_blank" href="https://yourlastrites.com/">
-              Last Rites
-            </a>
-            <br />
-            <a target="_blank" href="https://cvltnation.com/">
-              CVLTNation
-            </a>
-            <br />
-            <a target="_blank" href="https://noobheavy.com/">
-              Noob Heavy
-            </a>
-            <br />
-            <a target="_blank" href="https://toiletovhell.com/">
-              Toilet ov Hell
-            </a>
-            <br />
-            <a target="_blank" href="https://www.smallalbums.com/">
-              Small Albums
-            </a>
-            <br />
-            <a target="_blank" href="https://machinemusic.net/">
-              Machine Music
-            </a>
-            <br />
-            <a target="_blank" href="https://fortherabbits.net/">
-              For the Rabbits
-            </a>
-            <br />
-            <a target="_blank" href="https://www.goldflakepaint.co.uk/">
-              Gold Flake Paint
-            </a>
-            <br />
-            <a target="_blank" href="https://www.angrymetalguy.com/">
-              Angry Metal Guy
-            </a>
-            <br />
-            <a target="_blank" href="https://echoesanddust.com/">
-              Echoes and Dust
-            </a>
-            <br />
-            <a target="_blank" href="https://www.nocleansinging.com/">
-              No Clean Singing
-            </a>
-            <br />
-            <a target="_blank" href="https://www.invisibleoranges.com/">
-              Invisible Oranges
-            </a>
-            <br />
-            <a target="_blank" href="https://www.teethofthedivine.com/">
-              Teeth of the Divine
-            </a>
-            <br />
-            <a target="_blank" href="https://everythingisnoise.net/">
-              Everything Is Noise
-            </a>
-            <br />
-            <a target="_blank" href="https://www.heavyblogisheavy.com/">
-              Heavy Blog Is Heavy
-            </a>
-            <br />
-            <a target="_blank" href="https://varioussmallflames.co.uk/">
-              Various Small Flames
-            </a>
-            <br />
-            <a target="_blank" href="https://canthisevenbecalledmusic.com/">
-              Can This Even Be Called Music?
-            </a>
+
+            {pals.map((pal) => (
+              <div key={pal.name}>
+                <a href={pal.url} target="_blank">
+                  {pal.name}
+                </a>
+              </div>
+            ))}
           </div>
           <div>
             <h2 className="mb-4">Pals do best things with music.</h2>
-            <a target="_blank" href="https://twitter.com/KManriffs">
-              KManriffs
-            </a>
-            <br />
-            <a target="_blank" href="https://www.20buckspin.com/">
-              20 Buck Spin
-            </a>
-            <br />
-            <a target="_blank" href="http://audioantihero.com/">
-              Audio Antihero
-            </a>
-            <br />
-
-            <a target="_blank" href="https://weregnomerecords.bandcamp.com/">
-              WereGnome Records
-            </a>
-            <br />
-            <a target="_blank" href="http://sentientruin.com/main">
-              Sentient Ruin Laboratories
-            </a>
+            {others.map((pal) => (
+              <div key={pal.name}>
+                <a href={pal.url} target="_blank">
+                  {pal.name}
+                </a>
+              </div>
+            ))}
           </div>
         </div>
       </div>
