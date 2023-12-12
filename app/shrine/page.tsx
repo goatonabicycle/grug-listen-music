5b6d67f1c1f869d8cebe76cf1f9ccfa8643aab09
Rowan <malice.peanutbutter@gmail.com>
2023-12-12 22:22:14 +0200

    Bull init All shrine improvement.

--- app/shrine/page.tsx
+++ app/shrine/page.tsx
@@ -1,11 +1,8 @@
-import BlogCard from "@/components/blog-card";
 import BlogPageLayout from "@/components/layout/blog-page-layout";
-import Link from "@/components/link";
 import PostPaginator from "@/components/post-paginator";
-import RenderPosts from "@/components/render-posts";
 import { allSortedBlogs } from "@/lib/contentlayer";
 import config from "@/lib/siteConfig";
-import { cn, generateCommonMeta } from "@/lib/utils";
+import { generateCommonMeta } from "@/lib/utils";
 import { Metadata } from "next";
 
 export const metadata: Metadata = generateCommonMeta({
@@ -23,35 +20,14 @@ const Page = () => {
       </main>
     );
 
-  const blogs = [...allSortedBlogs];
-  const recentBlogs = blogs.splice(0, 3);
-  const latestPost = recentBlogs.shift();
-  const allPostsCount = config.blog.postPerPage - 3;
+  const shrines = [...allSortedBlogs];
+  const allPostsCount = config.blog.postPerPage;
 
   return (
     <main>
-      <BlogPageLayout title="Recent shrines">
-        {latestPost && (
-          <BlogCard
-            key={latestPost.artist + " - " + latestPost.album}
-            artist={latestPost.artist}
-            album={latestPost.album}
-            desc={latestPost.description}
-            tags={latestPost.tags}
-            date={latestPost.date}
-            img={latestPost.image}
-            href={`/shrine/${latestPost.slug}`}
-            priority
-          />
-        )}
-        <RenderPosts posts={recentBlogs} />
-      </BlogPageLayout>
-
-      <hr className="border-borders my-8 lg:my-12" />
-
-      {blogs.length > 0 && (
+      {shrines.length > 0 && (
         <BlogPageLayout title="All Shrines">
-          <PostPaginator posts={blogs} postPerPage={allPostsCount} page={1} />
+          <PostPaginator posts={shrines} postPerPage={allPostsCount} page={1} />
         </BlogPageLayout>
       )}
     </main>
