b4dcd231fcb88d2b160b05dad39282134f2bb68e
Rowan <malice.peanutbutter@gmail.com>
2024-03-11 22:57:24 +0200

    Convulsing progress!

--- next.config.js
+++ next.config.js
@@ -14,11 +14,6 @@ const nextConfig = {
     ],
   },
   modularizeImports: {
-    // TODO: https://github.com/iconoir-icons/iconoir/issues/243
-    // "iconoir-react": {
-    //   transform: "iconoir-react/dist/esm/server/{{member}}",
-    //   preventFullImport: true,
-    // },
     "@icons-pack": {
       transform: "@icons-pack/react-simple-icons/icons/{{member}}",
       preventFullImport: true,
