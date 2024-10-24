f543e230824e6774326819c94e81f0bd2179b025
Rowan <malice.peanutbutter@gmail.com>
2024-10-24 22:58:03 +0200

    Shouldn't work added

--- app/secret/page.tsx
+++ app/secret/page.tsx
@@ -5,8 +5,20 @@ const Secret = () => {
         <p className="mb-4">Secret embed yay!</p>
 
         <div className="flex justify-center">
+          Should work:
           <iframe
             src="https://ampwall.com/services/PlayerCard/v1/content/exclusive?embedId=0192c035-5f68-7650-a225-97db82245d75&controlStyle=persistent&playerWidth=350"
+            style={{
+              width: "350px",
+              minWidth: "350px",
+              height: "501px",
+              minHeight: "492px",
+            }}></iframe>
+          <br />
+          <br />
+          Should not work:
+          <iframe
+            src="https://ampwall.com/services/PlayerCard/v1/content/exclusive?embedId=0192c051-6f2b-7f70-85b9-59f6900c3038&controlStyle=persistent&playerWidth=350"
             style={{
               width: "350px",
               minWidth: "350px",
