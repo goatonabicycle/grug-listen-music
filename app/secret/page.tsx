6f60392df27167900340b6bb7a98537ef878131b
Rowan <malice.peanutbutter@gmail.com>
2024-10-24 22:34:46 +0200

    yay

--- app/secret/page.tsx
+++ app/secret/page.tsx
@@ -2,7 +2,7 @@ const Secret = () => {
   return (
     <div className="min-h-screen flex flex-col gap-4 justify-center items-center">
       <div className="px-10 py-20 text-center w-full">
-        <p className="mb-4">Secret embed soon!</p>
+        <p className="mb-4">Secret embed yay!</p>
         <iframe
           src="https://ampwall.com/services/PlayerCard/v1/content/exclusive?embedId=0192c035-5f68-7650-a225-97db82245d75&controlStyle=persistent&playerWidth=350"
           style={{
