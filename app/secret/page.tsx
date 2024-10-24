388a5b2acf45419c004420d33e9af1364ff0d545
Rowan <malice.peanutbutter@gmail.com>
2024-10-24 23:03:03 +0200

    Next to each other.

--- app/secret/page.tsx
+++ app/secret/page.tsx
@@ -4,27 +4,32 @@ const Secret = () => {
       <div className="px-10 py-20 text-center w-full">
         <p className="mb-4">Secret embed yay!</p>
 
-        <div className="flex justify-center">
-          Should work:
-          <iframe
-            src="https://ampwall.com/services/PlayerCard/v1/content/exclusive?embedId=0192c035-5f68-7650-a225-97db82245d75&controlStyle=persistent&playerWidth=350"
-            style={{
-              width: "350px",
-              minWidth: "350px",
-              height: "501px",
-              minHeight: "492px",
-            }}></iframe>
-          <br />
-          <br />
-          Should not work:
-          <iframe
-            src="https://ampwall.com/services/PlayerCard/v1/content/exclusive?embedId=0192c051-6f2b-7f70-85b9-59f6900c3038&controlStyle=persistent&playerWidth=350"
-            style={{
-              width: "350px",
-              minWidth: "350px",
-              height: "501px",
-              minHeight: "492px",
-            }}></iframe>
+        <div className="flex flex-col md:flex-row gap-8 justify-center items-center">
+          <div className="flex flex-col items-center">
+            <p className="mb-2">Should work:</p>
+            <iframe
+              src="https://ampwall.com/services/PlayerCard/v1/content/exclusive?embedId=0192c035-5f68-7650-a225-97db82245d75&controlStyle=persistent&playerWidth=350"
+              style={{
+                width: "350px",
+                minWidth: "350px",
+                height: "501px",
+                minHeight: "492px",
+              }}
+            ></iframe>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <p className="mb-2">Should not work:</p>
+            <iframe
+              src="https://ampwall.com/services/PlayerCard/v1/content/exclusive?embedId=0192c051-6f2b-7f70-85b9-59f6900c3038&controlStyle=persistent&playerWidth=350"
+              style={{
+                width: "350px",
+                minWidth: "350px",
+                height: "501px",
+                minHeight: "492px",
+              }}
+            ></iframe>
+          </div>
         </div>
       </div>
     </div>
