1b95981ea4eaa8410d946ad5dbd15004d5a1645e
Rowan <malice.peanutbutter@gmail.com>
2024-10-24 22:34:31 +0200

    Dammit linting.

--- app/secret/page.tsx
+++ app/secret/page.tsx
@@ -3,13 +3,14 @@ const Secret = () => {
     <div className="min-h-screen flex flex-col gap-4 justify-center items-center">
       <div className="px-10 py-20 text-center w-full">
         <p className="mb-4">Secret embed soon!</p>
-        <iframe src="https://ampwall.com/services/PlayerCard/v1/content/exclusive?embedId=0192c035-5f68-7650-a225-97db82245d75&controlStyle=persistent&playerWidth=350" 
-         style={{
-          width: '350px',
-          minWidth: '350px',
-          height: '502px',
-          minHeight: '492px'
-        }}></iframe>
+        <iframe
+          src="https://ampwall.com/services/PlayerCard/v1/content/exclusive?embedId=0192c035-5f68-7650-a225-97db82245d75&controlStyle=persistent&playerWidth=350"
+          style={{
+            width: "350px",
+            minWidth: "350px",
+            height: "502px",
+            minHeight: "492px",
+          }}></iframe>
       </div>
     </div>
   );
